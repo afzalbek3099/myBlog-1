9bcfa5eeeaaf8251d31088b0031bee3c9361984b
afzalbek3099 <afzalbekaxmadjonov3099@gmail.com>
2021-11-15 16:39:18 +0500

    asd

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const Maqola = require("../models/Maqola");
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-
   const maqola = await Maqola.find()
   res.render("index", { layout: "layout", maqola });
 });
@@ -12,9 +11,7 @@ router.get("/", async function (req, res, next) {
 
 
 
-
   res.render("index", { layout: "layout" });
 });
-
 
 module.exports = router;
